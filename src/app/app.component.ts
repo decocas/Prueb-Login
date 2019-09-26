4779b4200ce12f64f1cbb91e9366496c8b8d71c6
decocas <37914111+decocas@users.noreply.github.com>
2019-09-26 15:06:53 -0500

    con login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Prueb-Login';
+constructor(public af: AngularFireAuth){
+	
+} 
+    title = 'Login';
+
+    login() {
+    this.af.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.af.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +12,7 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
